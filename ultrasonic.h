e084f1a5411819ef7b400afd2e025e9243714def
Ashvanth-Raj25 <nghvharshan19274@gmail.com>
2025-08-28 21:03:59 +0530

    header added

---
+++ ultrasonic.h
@@ -0,0 +1,28 @@
+/* 
+ * File:   ultrasonic.h
+ * Author: ashva
+ *
+ * Created on 12 August, 2025, 10:50 AM
+ */
+
+#ifndef ULTRASONIC_H
+#define	ULTRASONIC_H
+void timer();
+void display(int);
+void command(char,char);
+void enable();
+void delay(unsigned int);
+#endif	/* ULTRASONIC_H */
+#ifdef	__cplusplus
+extern "C" {
+#endif
+
+
+
+
+#ifdef	__cplusplus
+}
+#endif
+
+
+
